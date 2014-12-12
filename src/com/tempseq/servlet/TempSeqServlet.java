068126a20e51544d7a5968e0b776c2b2d4bc6e0d
Aleksi Pekki <aleksi.pekki@gmail.com>
2014-12-12 19:04:00 +0200

    display measurements

--- src/com/tempseq/servlet/TempSeqServlet.java
+++ src/com/tempseq/servlet/TempSeqServlet.java
@@ -9,6 +9,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+
 /**
  * Servlet implementation class TempSeqServlet
  */
@@ -30,8 +35,28 @@ public class TempSeqServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
+		Cluster cluster = Cluster.builder().addContactPoints("localhost").build();
+		
+		Session session = cluster.connect();
+
+		String queryString = "SELECT location_id, datetime, temperature FROM temp_seq.measurements";
+		ResultSet result = session.execute(queryString);
+		
 		PrintWriter out = response.getWriter();
+		out.println("<html><head></head><body>");
 		out.println("Temperature sequence");
+		out.println("<table>");
+
+		for (Row row : result)
+		{
+		   out.println("<tr>");
+		   out.println("<td>" + row.getString("location_id") + "</td>");
+		   out.println("<td>" + row.getDate("datetime") + "</td>");
+		   out.println("<td>" + row.getDouble("temperature") + "</td>");   
+		   out.println("</tr>");
+		}
+		out.println("</table>");
+		out.println("</body></html>");
 	}
 
 }
